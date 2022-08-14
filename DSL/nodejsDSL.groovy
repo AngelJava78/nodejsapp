778ef81feb3e9646cbf9c3c6669bf4a6a5856cf6
AngelJava78 <angel_java@hotmail.com>
2022-08-14 05:16:13 -0500

    Cambio de repositorio

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/AngelJava78/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('AngelJava78')
+            node / gitConfigEmail('angel_java@hotmail.com')
         }
     }
     triggers {
